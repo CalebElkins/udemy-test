e468cc56ee56bc41eeb0f08e6622b1a504463b92
Caleb <cmelkins@live.com>
2017-11-10 19:44:35 -0600

    Added 2nd loop to Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Github");
-		
-		//Added code from eclipse
-		for(int i = 0; i < 10; i++){
+
+		// Added code from eclipse
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+
+		// Added code from eclipse; 2nd loop
+		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
 	}
